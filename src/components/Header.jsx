56005ed7bb83cfbb5c9c566af4bc771ae743886e
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-19 13:04:46 +0100

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+
+
+    return(
+        <>
+        
+        <h1>Benvenuto nei nostri film</h1>
+
+        <input  type="placeholder" />
+        </>
+    )
+
+    
+}
