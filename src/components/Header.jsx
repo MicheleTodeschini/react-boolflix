f32539bff286c0af0bf4355fcbc0017798ba9ca4
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-20 11:36:51 +0100

    add map, icon for the flag and modified the url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react"
+import axios from "axios"
+
 export default function Header() {
 
+    const base_movie_api_url = 'https://api.themoviedb.org/3/search/movie'
+    const [search, setSearch] = useState('')
+    const [movies, setMovies] = useState(null)
+
+    function handleSubmit() {
+        e.preventDefault()
+        console.log(search);
+        const endpoint = `${base_movie_api_url}?api_key=${import.meta.env.VITE_THE_MOVIE_DB_API_KEY}&query=${search}`
+        
+        axios.get(endpoint)
+        .then(res=> setMovies(res.data))
+    }
+
+        
 
     return(
         <>
         
         <h1>Benvenuto nei nostri film</h1>
+        <form onSubmit={handleSubmit}>
 
-        <input  type="placeholder" />
+        <input  type="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button >Cerca</button>
+
+        </form>
+
+        <ul>
+            {
+                movies?.res.map(movie =>
+                    <li>
+                       <p>{movie.title} </p> 
+                       <p>{movie.original_language}</p> <span class={`fi fi-${movie.original_language}`}></span>
+                    
+                    </li>
+                )
+            }
+        </ul>
         </>
     )
 
